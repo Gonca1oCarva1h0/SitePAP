d232d4ebe3365ae5f867f55905be68d694706cb6
Gonca1oCarva1h0 <goncaloc770@gmail.com>
2023-11-03 09:36:19 +0000

    Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const downloadButton = document.getElementById("download");
     const downloadLink = document.getElementById("download-link");
 
-    // Defina o atributo 'href' do link de download para o arquivo que você deseja baixar.
-    downloadLink.href = "gameFille/FilipeOAventureiro.zip";; // Substitua pelo caminho real do arquivo.
+    downloadLink.href = "FilipeOAventureiro.zip";;
 
     downloadButton.addEventListener("click", function () {
 
