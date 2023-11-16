367dd1ec061bdd6356614ed538b8f692d2367375
G0NCA1O <goncaloc770@gmail.com>
2023-11-16 22:29:33 +0000

    Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,10 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const downloadButton = document.getElementById("download");
     const downloadLink = document.getElementById("download-link");
 
-    downloadLink.href = "FilipeOAventureiro.zip";;
+    downloadLink.href = "FilipeOAventureiro.zip";
 
     downloadButton.addEventListener("click", function () {
-
         downloadLink.click();
     });
 });
+
+window.onload = function () {
+    window.scrollTo({
+        top: document.querySelector('#inicio').offsetTop,
+        behavior: 'smooth'
+    });
+};
